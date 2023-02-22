1f650730b0272b853bc17d9b9a1816b68c46dd22
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2023-02-22 13:16:48 -0500

    add shell wrapper to run tests

---
+++ dev/run_pkg_tests.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# runs test for rtapas
+
+# set defaults
+cd $(dirname $0)
+APPENDLOG=0
+
+function run_test_script () {
+    test_script=$1
+
+    echo "Performing test on $test_script" 
+
+    # Run test and redirect output (append or overwrite)
+    if [ ! ${APPENDLOG} -eq 1 ]; then
+        Rscript $test_script 1> /dev/null 2> pkg_tests.err
+        APPENDLOG=1
+    else
+        Rscript $test_script 1> /dev/null 2>> pkg_tests.err
+    fi
+    
+    if [ ! $? -eq 0 ]; then
+        echo "Performing test on $test_script (FAILED)"
+        exit $?
+
+    else
+        echo "Performing test on $test_script (SUCCEEDED)"
+        exit 0
+    fi
+}
+
+run_test_script test_rtapas.R 
